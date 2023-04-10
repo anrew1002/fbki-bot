861d917d518ee11d1287f786e627676cf844fd12
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2023-04-10 16:20:46 +0800

    render_template

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template("profile.html")
 
 
 if __name__ == "__main__":
