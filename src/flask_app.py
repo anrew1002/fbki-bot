615753db4019be32de85894d6e64194656f17d1c
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2023-05-16 14:41:59 +0800

    try fix uwsgi

--- src/flask_app.py
+++ src/flask_app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-from auth import Auth
 app = Flask(__name__)
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from src.flask_app import app
 
 if __name__ == "__main__":
+    from src.auth import Auth
     app.run()
